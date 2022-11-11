ddaae558a69db80c9d0b3b3f80469df9e812aa77
Adrian Gaile Singh <adriangailee@gmail.com>
2022-11-12 01:55:16 +0800

    Add page for adding students

---
+++ add-student.php
@@ -0,0 +1,18 @@
+<?php include('includes/header.php');?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>
+                        ARFUN Crud - Realtime Database
+                        <a href="index.php" class="btn btn-danger float-end">Back</a></h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php');?>
+    

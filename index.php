0d810446e99140ce58875ea24ec7ab805e17247d
Adrian Gaile Singh <adriangailee@gmail.com>
2022-11-12 01:55:50 +0800

    add button for adding students

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include('includes/header.php');?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>
+                        ARFUN Crud - Realtime Database
+                        <a href="add-student.php" class="btn btn-primary float-end">Add Students</a></h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php');?>
+    

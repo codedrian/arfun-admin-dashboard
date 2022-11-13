1663f896408a1b442dc702768bfe8dae99b4d5d5
Adrian Gaile Singh <adriangailee@gmail.com>
2022-11-13 17:33:08 +0800

    Move the student table to manage-student.php

--- add-student.php
+++ add-student.php
@@ -7,7 +7,26 @@
                 <div class="card-header">
                     <h4>
                         ARFUN Crud - Realtime Database
-                        <a href="index.php" class="btn btn-danger float-end">Back</a></h4>
+                        <a href="manage-student.php" class="btn btn-danger float-end">Back</a></h4>
+                </div>
+                <div class="card-body ">
+                    <form action="code.php" method="POST">
+                        <div class="form-group mb-3">
+                            <label for="first_name">First Name:</label>
+                            <input type="text" name="first_name" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="last_name">Last Name:</label>
+                            <input type="text" name="last_name" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="save" class="btn btn-primary primary"><a href="manage-student.php"></a>Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>

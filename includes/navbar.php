1e2f36935b0c3a39ab747b41ad2b1101927455bf
Adrian Gaile Singh <adriangailee@gmail.com>
2022-11-20 22:51:54 +0800

    Update href links

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
 <div class="container">
-  
-    <a class="navbar-brand text-white" href="#">Navbar</a>
+    <a class="navbar-brand text-white" href="#">ARFUN E-Learning Admin Panel</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -11,16 +10,16 @@
     <ul class="navbar-nav me-auto ms-auto mb-2 mb-lg-0 ">
       
       <li class="nav-item">
-        <a class="nav-link " href="#">Home</a>
+        <a class="nav-link " href="index.php">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link " href="#">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link " href="">Register</a>
+        <a class="nav-link " href="register.php">Register</a>
       </li>
        <li class="nav-item">
-        <a class="nav-link " href="#">Login</a>
+        <a class="nav-link " href="adminLogin.php">Login</a>
       </li>
     </ul>
     </div>

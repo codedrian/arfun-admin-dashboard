94d90238ea674cfdf292351adf5316d5880d6e56
Adrian Gaile Singh <adriangailee@gmail.com>
2022-11-22 01:20:57 +0800

    Add a condition that if you're already looged in you can't access login page

--- includes/navbar.php
+++ includes/navbar.php
@@ -15,12 +15,20 @@
       <li class="nav-item">
         <a class="nav-link " href="#">About</a>
       </li>
-      <li class="nav-item">
+
+      <?php if (!isset($_SESSION['verified_user_id'])) : ?>
+      <!-- <li class="nav-item">
         <a class="nav-link " href="register.php">Register</a>
-      </li>
+      </li> -->
        <li class="nav-item">
         <a class="nav-link " href="adminLogin.php">Login</a>
       </li>
+      
+      <?php   else : ?>
+      <li class="nav-item">
+        <a class="nav-link bg-danger" href="logout.php">Logout</a>
+      </li>  
+      <?php endif; ?>
     </ul>
     </div>
   </div>

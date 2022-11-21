19c21a50ec88a62856a85c954e87f3b9cdd6da64
Adrian Gaile Singh <adriangailee@gmail.com>
2022-11-22 01:02:19 +0800

    Add a isset function to login button and to check if Email is in the Firebase Auth using try and catch

--- adminLoginCode.php
+++ adminLoginCode.php
@@ -2,20 +2,53 @@
 session_start();
 include('dbcon.php'); //for database connection
 
-//METHOD
+
+//METHOD to check if the button is set or not
 if(isset($_POST['login_button'])){
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $clearTextPassword = $_POST['password'];
 
-    try {
-    $user = $auth->getUserByEmail($email);
-    $user = $auth->getUserByPhoneNumber('+49-123-456789');
-} catch (\Kreait\Firebase\Exception\Auth\UserNotFound $e) {
-    echo $e->getMessage();
-}
+    try { // to check if the email is in the Firebase Authentication
+        $user = $auth->getUserByEmail("$email"); 
+
+        try {
+            $signInResult = $auth->signInWithEmailAndPassword($email, $clearTextPassword);
+            $idTokenString  = $signInResult->idToken(); // to verify firebase token
+
+            try {
+                $verifiedIdToken = $auth->verifyIdToken($idTokenString);
+                $uid = $signInResult->firebaseUserId(); // to get the user id
+
+                $_SESSION['verified_user_id'] = $uid;
+                $_SESSION['idTokenString'] = $idTokenString; //store ID token into session
+
+                $_SESSION['status'] = "Logged in Succesfuly";
+                header('Location: index.php');
+                exit();
+                
+            } catch (Exception $e) {
+                echo 'The token is invalid: '.$e->getMessage();
+                
+            }catch(\InvalidArgumentException $e) {
+                echo 'The token could not be parsed: '.$e->getMessage();
+            }
+
+        } catch(Exception $e) {
+            $_SESSION['status'] = "Login Failed: Please enter correct password";
+            header('Location: adminLogin.php');
+            exit();
+        }
+
+    } catch (\Kreait\Firebase\Exception\Auth\UserNotFound $e) {
+    // echo $e->getMessage();
+    $_SESSION['status'] = "Login Failed: Please enter correct email";
+    header('Location: adminLogin.php');
+    exit();
+    }
+
 }
 else {
-    $_SESSION = "Login Failed: Please enter email and password";
+    $_SESSION['status'] = "Login Failed: Please enter email and password";
     header('Location: adminLogin.php');
     exit();
 }

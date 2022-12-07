55044e9326605e31b79d22c4892215dee42ec8bb
xenz25 <zianjolo.catacutan.m.dev@gmail.com>
2022-12-08 01:18:45 +0800

    Added: +question, dynamic template, -question

--- js/mkquiz.js
+++ js/mkquiz.js
@@ -1,31 +1,23 @@
-function getChoiceTemplate(qid) {
-  return `<div class="col d-flex">
-    <div class="input-group d-flex align-items-center">
-        <div class="form-check me-2">
-            <input class="form-check-input" type="radio" name="choice-${qid}"
-                id="choice">
-        </div>
-        <input type="text" class="form-control"
-            placeholder="Choice value">
-        <a class="btn btn-danger" onClick="removeChoice(this)">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                height="16" fill="currentColor" class="bi bi-dash"
-                viewBox="0 0 16 16">
-                <path
-                    d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
-            </svg>
-        </a>
-    </div>
-</div>`;
+var questionContainer = document.querySelector(".question-container");
+
+function init() {
+  var tempDiv = document.createElement("div");
+  tempDiv.innerHTML = getQuestionTemplate(uuidv4(), false);
+  questionContainer.append(tempDiv.firstElementChild);
 }
 
+// create default question template
+init();
+
 // use for adding choice base on questionId
-function addChoice(qid) {
+function addChoice(e) {
+  var qid =
+    e.parentElement.parentElement.parentElement.getAttribute("data-qid");
   var container = document.querySelector(`[data-choice-qid="${qid}"]`);
   var tempDiv = document.createElement("div");
-  tempDiv.innerHTML = getChoiceTemplate(qid);
+  tempDiv.innerHTML = getChoiceTemplate(qid, false, true);
 
-  container.append(tempDiv.firstChild);
+  container.append(tempDiv.firstElementChild);
 }
 
 function removeChoice(e) {
@@ -37,4 +29,27 @@ function removeChoice(e) {
     ).checked = true;
   }
   inputGroupParent.parentElement.remove();
+}
+
+function addQuestion() {
+  var tempDiv = document.createElement("div");
+  tempDiv.innerHTML = getQuestionTemplate(uuidv4(), true);
+  questionContainer.append(tempDiv.firstElementChild);
+}
+
+function removeQuestion(qid) {
+  var confirmDeletion = confirm("This question will be deleted?");
+  if (confirmDeletion) {
+    document.querySelector(`[data-qid="${qid}"]`).remove();
+  }
+}
+
+// create guids v4 - base on: https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+function uuidv4() {
+  return ([1e7] + 1e3 + 4e3 + 8e3 + 1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
 }

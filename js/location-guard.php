201cd138117e91cb21c20d41255eeb33cd5f2df5
Adrian Gaile Singh <adriangailee@gmail.com>
2023-01-14 12:27:39 +0800

    change add-student.php to student-list.php on  adminExPaths

--- js/location-guard.php
+++ js/location-guard.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['location'])) {
     $role = $_SESSION['role'];
 
     // $adminExPaths = array("lesson.php", "create-quiz.php");
-    $adminExPaths = array("index.php", "register.php", "add-student.php", "add-teacher.php", "user-list.php");
+    $adminExPaths = array("index.php", "register.php", "student-list.php", "add-teacher.php", "user-list.php");
     // $teacherExPaths = array("register.php", "add-teacher.php", "user-list.php");
     $teacherExPaths = array("index.php", "add-student.php", "lesson.php", "create-quiz.php");
 

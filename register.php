f7978d94b7d13ac98fb6c53b3e73581c1e15ed09
Adrian Gaile Singh <adriangailee@gmail.com>
2023-01-16 21:18:35 +0800

    refactor: remove admin phone number

--- register.php
+++ register.php
@@ -120,12 +120,7 @@
                                                         <input type="text" class="form-control" id="email"
                                                             placeholder="Email" name="email" required>
                                                     </div>
-
-                                                    <div class="form-group col-xs-4">
-                                                        <label for=""></label>
-                                                        <input type="text" class="form-control" id="phone"
-                                                            placeholder="Phone" name="phone" required>
-                                                    </div>
+                            
 
                                                     <button type="button"
                                                         class="btn btn-primary registerbtn mt-4 btn-sm"
